dfd48e4aa760181186caec659e3aea5b197e6760
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2021-04-15 20:59:35 +0200

    tested get and post on local

--- main.py
+++ main.py
@@ -4,12 +4,19 @@ from flask_restful import Api, Resource
 app = Flask(__name__)
 
 api = Api(app)
+food = {"Lu": {"age": 5, "gender": "male"},
+        "Lou": {"age": 3, "gender": "female"}}
 
 
-class Hello(Resource):
-    def get(self):
-        return {"hello world"}
+class Food(Resource):
+    def get(self, name):
+        return food[name]
 
+    def post(self):
+        return {"data": "Posted"}
+
+
+api.add_resource(Food, "/cats/<string:name>")
 
 if __name__ == '__main__':
     app.run(debug=True)
