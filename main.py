0ce12135603028f2430ab781887a6435cacfa28b
Fabian Kowatsch <f.kowatsch2000@gmail.com>
2021-04-17 11:41:29 +0200

    created db

--- main.py
+++ main.py
@@ -1,30 +1,62 @@
 from flask import Flask
-from flask_restful import Api, Resource, reqparse
+from flask_restful import Api, Resource, reqparse, abort
+from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-
 api = Api(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///tmp/database.db'
+db = SQLAlchemy(app)
+
+
+class FoodModel(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    date_added = db.Column(db.String(100), nullable=True)
+    date_expires = db.Column(db.String(100), nullable=True)
+
+    def __repr__(self):
+        return f"Food(name={name}, date_added={date_added}, date_expires={date_expires}"
+
 
 food_put_args = reqparse.RequestParser()
-food_put_args.add_argument("name", type=str, help="name of the food")
+food_put_args.add_argument(
+    "name", type=str, help="name of the food is required", required=True)
 food_put_args.add_argument("date_added", type=str,
                            help="date of the added food")
-food_put_args.add_argument("date_added", type=str,
-                           help="date of the added food")
+food_put_args.add_argument("date_expires", type=str,
+                           help="estimated expire date of the food")
 
 food = {}
 
 
 class Food(Resource):
-    def get(self, name):
-        return food[name]
+    def get(self, food_id):
+        abort_food_id_not_available(food_id)
+        return food[food_id]
 
-    def put(self, name):
+    def put(self, food_id):
+        abort_food_id_exists(food_id)
         args = food_put_args.parse_args()
-        return {name: args}
+        food[food_id] = args
+        return food[food_id], 201
+
+    def delete(self, food_id):
+        abort_food_id_not_available(food_id)
+        del food[food_id]
+        return {"deleted": str(food_id)}, 204
 
 
-api.add_resource(Food, "/cats/<string:name>")
+def abort_food_id_not_available(food_id):
+    if food_id not in food:
+        abort(404, message="food_id does not exist")
+
+
+def abort_food_id_exists(food_id):
+    if food_id in food:
+        abort(409, message="food_id already exists")
+
+
+api.add_resource(Food, "/food/<int:food_id>")
 
 if __name__ == '__main__':
     app.run(debug=True)
--- test.py
+++ test.py
@@ -2,5 +2,17 @@ import requests
 
 BASE = "http://127.0.0.1:5000/"
 
-response = requests.put(BASE + "/cats/Lou", {"date": "10.01.21"})
+data = [{"name": "essen1", "date_added": "10.01.21"},
+        {"name": "essen2", "date_added": "10.01.21"},
+        {"name": "essen3", "date_added": "10.01.21"}]
+
+
+for i in range(len(data)):
+    response = requests.put(
+        BASE + "/food/" + str(i), data[i])
+    print(response.json())
+
+response = requests.delete(BASE + "/food/2")
+print(response)
+response = requests.get(BASE + "/food/1")
 print(response.json())
